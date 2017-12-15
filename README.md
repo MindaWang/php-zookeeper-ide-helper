7241242c0da4fd83f0a6825d43fa018066c75e36
wangminda <wangminda@bilibili.com>
2017-12-15 19:05:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# php-zookeeper-ide-helper
---
+++ php-zookeeper-ide-helper.php
@@ -0,0 +1,32 @@
+<?php
+class Zookeeper {
+
+    public static function addAuth {
+
+    }
+    public static function connect {
+        
+    }
+    public static function create {
+        
+    }
+    public static function delete  {
+        
+    }
+    public static function exists  {
+        
+    }
+    public static function get  {
+        
+    }
+    public static function getChildren   {
+        
+    }
+    public static function getRecvTimeout  {
+        
+    }
+    public static function getState  {
+        
+    }
+    public static function set
+}
